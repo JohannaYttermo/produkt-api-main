438fb41237f09f4bb6c82bf8a9167adfe8169aa7
Johanna Yttermo <112933471+JohannaYttermo@users.noreply.github.com>
2023-08-03 13:43:38 +0200

    Komplettering tester

--- src/test/java/com/example/produktapi/service/ProductServiceTest_2.java
+++ src/test/java/com/example/produktapi/service/ProductServiceTest_2.java
@@ -59,7 +59,9 @@ class ProductServiceTest_2 {
         verify(repository,times(1)).findByCategory("electronic");
     }
 
-   @Test
+
+    // Komplettering - getProductById
+    @Test
     void getProductById() {
         // Given
          int id = 5;
@@ -76,6 +78,21 @@ class ProductServiceTest_2 {
 
     }
 
+    @Test
+    void whenGetProductById_withNonExistingId_thenShouldReturnNull() {
+        // given
+        int nonExistingId = 999; // Ett ID som inte finns i databasen
+        when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        // when
+        Product result = underTest.getProductById(nonExistingId);
+
+        // then
+        assertNull(result); // Förväntat resultat är att metoden returnerar null när produkten inte finns i databasen.
+    }
+
+
+
 
     @Test
     void whenAddingAProduct_thenReturnTrueIfProductIsPresent() {
@@ -90,6 +107,48 @@ class ProductServiceTest_2 {
         assertTrue(repository.findById(5).isPresent());  // ser så att produkt med ID 5 finns (isPresent)
 
     }
+
+
+    //komplettering  addProduct felflöde
+    @Test
+    void testAddingAProductInvalidId() {
+        // Given
+        Product product = new Product("Dator", 4000.0, "elektronik", "", "");
+        underTest.addProduct(product);
+        product.setId(5);
+
+        // When
+        // Försök hitta en produkt med ett ogiltigt ID
+        when(repository.findById(8)).thenReturn(Optional.empty());
+
+        // Then
+        assertFalse(repository.findById(8).isPresent());
+    }
+
+// komplettering för addProduct
+    @Test
+     void AddProduct() {
+        // Given
+        Product product = new Product("Dator", 4000.0, "elektronik", "", "");
+
+        // Förbered argumentCaptor för att fånga upp värdet som skickas till repository:s save-metod
+        ArgumentCaptor<Product> argumentCaptor = ArgumentCaptor.forClass(Product.class);
+
+        // When
+        underTest.addProduct(product);
+
+        // Then
+        // Verifiera att repository:s save-metod anropas med rätt argument
+        verify(repository, times(1)).save(argumentCaptor.capture());
+
+        // Hämta fångade värdet
+        Product capturedProduct = argumentCaptor.getValue();
+
+        // Jämför att fångade värdet stämmer överens med det värde vi skickade in i addProduct-metoden
+        assertEquals(product, capturedProduct);
+    }
+
+
 
 
     @Test
@@ -114,8 +173,27 @@ class ProductServiceTest_2 {
 
     }
 
+
+    //komplettering - felflöde för updateProduct
     @Test
-    void deleteProduct() {
+    void whenUpdateAProduct_withNonExistingId_thenShouldReturnNull() {
+        // given
+        int nonExistingId = 999; // Ett ID som inte finns i databasen
+        Product updatedProduct = new Product("Dator", 26.000, "elektronik", "bra", "url");
+        when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        // when
+        Product result = underTest.updateProduct(updatedProduct, nonExistingId);
+
+        // then
+        assertNull(result); // Förväntat resultat är att metoden returnerar null när produkten inte finns i databasen.
+    }
+
+
+
+
+    @Test
+    void deleteProduct() {  // Metod som anropar deleteById
 
         //Given
         int id = 10;
@@ -131,4 +209,20 @@ class ProductServiceTest_2 {
 
         // assertNull(underTest.getProductById(id));
     }
+
+
+    // Komplettering - DeleteProduct felflöde
+    @Test
+    void whenDeleteProduct_withNonExistingId_thenShouldNotCallDeleteById() {
+        // given
+        int nonExistingId = 999; // Ett ID som inte finns i databasen
+        when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        // when
+        underTest.deleteProduct(nonExistingId);
+
+        // then
+        verify(repository, times(0)).deleteById(nonExistingId); // Förväntar oss att deleteById-metoden inte anropas
+    }
+
 }
